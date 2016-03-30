34299a7b0294ce0558c4ee5e9b9a571667564e32
mervyn.chung <mervyn.chung@foxmail.com>
2016-03-30 20:39:07 +0800

    修改了compBaseTable组件

--- src/component/compBaseTable/index.jsx
+++ src/component/compBaseTable/index.jsx
@@ -1,9 +1,10 @@
 /**
  * 单实体数据表格展示组件
- * @props.data {Object} 包含样式定义的数据
+ * @props.data {Object} 单个实体数据
+ * @props.model {Object} 描述实体的属性定义，以及每行显示的属性组数
  * @props.bordered {boolean} 是否有边框
  * @props.striped ｛boolean｝ 是否有间隔背景条纹
- * 参考用法: const demo = <BaseTable data={data} bordered striped/>
+ * 参考用法: const demo = <BaseTable data={data} model={model} bordered striped/>
  */
 import React from 'react'
 import ReactDom from 'react-dom'
@@ -11,52 +12,51 @@ import './style.css'
 
 
 /*数据样式
- const data = {
- values: {
- name: '胡延兵',
- age: '23',
- sex: '男',
- xl: '本科',
- cs: '广州',
- dz: '中山三路中山三路中山三路中山三路中山三路中山三路',
- byyx: '北京大学',
- zgzh: '34454394',
- barq: '2016-01-01'
- },
- colGroupNum: 2,
- keys: [{
- id: 'name',
- name: '姓名'
- }, {
- id: 'age',
- name: '年龄',
- }, {
- id: 'sex',
- name: '性别'
- }, {
- id: 'xl',
- name: '学历',
- groupspan: '2'
- }, {
- id: 'cs',
- name: '城市'
- }, {
- id: 'dz',
- name: '地址'
- }, {
- id: 'byyx',
- name: '毕业院校',
- groupspan: '2'
- }, {
- id: 'zgzh',
- name: '证书编号',
- groupspan: '2'
- }, {
- id: 'barq',
- name: '备案日期'
- }]
- }
- */
+const data = {
+    name: '胡延兵',
+    age: '23',
+    sex: '男',
+    xl: '本科',
+    cs: '广州',
+    dz: '中山三路',
+    byyx: '北京大学',
+    zgzh: '34454394',
+    barq: '2016-01-01'
+
+}
+const model = {
+    colGroupNum: 2,
+    props: [{
+        id: 'name',
+        name: '姓名'
+    }, {
+        id: 'age',
+        name: '年龄'
+    }, {
+        id: 'sex',
+        name: '性别'
+    }, {
+        id: 'xl',
+        name: '学历'
+    }, {
+        id: 'cs',
+        name: '城市'
+    }, {
+        id: 'dz',
+        name: '地址'
+    }, {
+        id: 'byyx',
+        name: '毕业院校'
+    }, {
+        id: 'zgzh',
+        name: '证书编号',
+        groupspan: 2
+
+    }, {
+        id: 'barq',
+        name: '备案日期'
+    }]
+}*/
 
 const TrWrapper = React.createClass({
     render(){
@@ -70,9 +70,9 @@ const baseTable = React.createClass({
         const colgroup = [];
         const tr = [];
         let td = [];
-        const colGroupNum = this.props.model.colGroupNum;
+        const colGroupNum = this.props.model.colGroupNum < 5 ? this.props.model.colGroupNum : 2
         //设置colgroup样式
-        for (let i = 0; i < (1 < colGroupNum < 5 ? colGroupNum : 2); i++) {
+        for (let i = 0; i < colGroupNum; i++) {
             let spanKey = 24 / (colGroupNum * 3);
             let spanValue = 24 * 2 / (colGroupNum * 3);
             if (colGroupNum == 3) {
@@ -92,20 +92,20 @@ const baseTable = React.createClass({
                     td = [];
                     colCount = 0;
                 }
-                td.push(<td prop={'td-k-'+prop.id} className="prop-name">{prop.name}</td>);
-                td.push(<td prop={'td-v-'+prop.id} colSpan={prop.groupspan*2-1}>{this.props.data[prop.id]}</td>);
+                td.push(<td key={'td-k-'+prop.id} className="prop-name">{prop.name}</td>);
+                td.push(<td key={'td-v-'+prop.id} colSpan={prop.groupspan*2-1}>{this.props.data[prop.id]}</td>);
                 colCount += prop.groupspan
                 //处理非跨列项目
             } else {
-                td.push(<td prop={'td-k-'+prop.id} className="prop-name">{prop.name}</td>);
-                td.push(<td prop={'td-v-'+prop.id}>{this.props.data[prop.id]}</td>);
+                td.push(<td key={'td-k-'+prop.id} className="prop-name">{prop.name}</td>);
+                td.push(<td key={'td-v-'+prop.id}>{this.props.data[prop.id]}</td>);
                 colCount += 1;
             }
             if (colCount == colGroupNum) {
                 tr.push(<TrWrapper key={'tr-'+tr.length+1}>{td}</TrWrapper>);
                 td = [];
                 colCount = 0;
-            } else if (i == this.props.model.length - 1) {
+            } else if (i == this.props.model.props.length - 1) {
                 tr.push(<TrWrapper key={'tr-'+tr.length+1}>{td}</TrWrapper>);
             }
         }
