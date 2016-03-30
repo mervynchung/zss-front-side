1bf8c5e3d8911718553177c314c714967b512685
mervyn.chung <mervyn.chung@foxmail.com>
2016-03-30 17:38:49 +0800

    20160330

--- src/component/compBaseTable/index.jsx
+++ src/component/compBaseTable/index.jsx
@@ -1,8 +1,17 @@
+/**
+ * 单实体数据表格展示组件
+ * @props.data {Object} 包含样式定义的数据
+ * @props.bordered {boolean} 是否有边框
+ * @props.striped ｛boolean｝ 是否有间隔背景条纹
+ * 参考用法: const demo = <BaseTable data={data} bordered striped/>
+ */
 import React from 'react'
 import ReactDom from 'react-dom'
 import './style.css'
 
-/*const data = {
+
+/*数据样式
+ const data = {
  values: {
  name: '胡延兵',
  age: '23',
@@ -17,7 +26,7 @@ import './style.css'
  colGroupNum: 2,
  keys: [{
  id: 'name',
- name: '姓名',
+ name: '姓名'
  }, {
  id: 'age',
  name: '年龄',
@@ -46,47 +55,57 @@ import './style.css'
  id: 'barq',
  name: '备案日期'
  }]
- }*/
+ }
+ */
+
 const TrWrapper = React.createClass({
     render(){
         return <tr>{this.props.children}</tr>
     }
 })
 
-const BaseTable = React.createClass({
+const baseTable = React.createClass({
     render(){
         let colCount = 0;
         const colgroup = [];
         const tr = [];
         let td = [];
-        const colGroupNum = this.props.data.colGroupNum;
+        const colGroupNum = this.props.model.colGroupNum;
         //设置colgroup样式
-        for (let i = 0; i < (colGroupNum < 3 ? colGroupNum : 2); i++) {
+        for (let i = 0; i < (1 < colGroupNum < 5 ? colGroupNum : 2); i++) {
             let spanKey = 24 / (colGroupNum * 3);
             let spanValue = 24 * 2 / (colGroupNum * 3);
+            if (colGroupNum == 3) {
+                spanKey = 3;
+                spanValue = 5;
+            }
             colgroup.push(<col key={'c-k-'+i} className={'col-'+spanKey}></col>);
             colgroup.push(<col key={'c-v-'+i} className={'col-'+spanValue}></col>);
         }
         //将实体内容以key:value放置到对应的td组中，再按照colGroupNum分列
-        for (let i = 0; i < this.props.data.keys.length; i++) {
-            let key = this.props.data.keys[i];
-            if(key.groupspan){
-                if(colGroupNum < colCount+key.groupspan ){
+        for (let i = 0; i < this.props.model.props.length; i++) {
+            let prop = this.props.model.props[i];
+            //处理跨列项目
+            if (prop.groupspan) {
+                if (colGroupNum < colCount + prop.groupspan) {
                     tr.push(<TrWrapper key={'tr-'+tr.length+1}>{td}</TrWrapper>);
-                    td=[];colCount=0;
+                    td = [];
+                    colCount = 0;
                 }
-                td.push(<td key={'td-k-'+key.id}>{key.name}</td>);
-                td.push(<td key={'td-v-'+key.id} colSpan={key.groupspan*2-1}>{this.props.data.values[key.id]}</td>);
-                colCount+=key.groupspan
-            }else{
-                td.push(<td key={'td-k-'+key.id}>{key.name}</td>);
-                td.push(<td key={'td-v-'+key.id}>{this.props.data.values[key.id]}</td>);
-                colCount+=1;
+                td.push(<td prop={'td-k-'+prop.id} className="prop-name">{prop.name}</td>);
+                td.push(<td prop={'td-v-'+prop.id} colSpan={prop.groupspan*2-1}>{this.props.data[prop.id]}</td>);
+                colCount += prop.groupspan
+                //处理非跨列项目
+            } else {
+                td.push(<td prop={'td-k-'+prop.id} className="prop-name">{prop.name}</td>);
+                td.push(<td prop={'td-v-'+prop.id}>{this.props.data[prop.id]}</td>);
+                colCount += 1;
             }
-            if (0 == colCount % colGroupNum ) {
+            if (colCount == colGroupNum) {
                 tr.push(<TrWrapper key={'tr-'+tr.length+1}>{td}</TrWrapper>);
-                td = [];colCount=0;
-            }else if(i==this.props.data.keys.length-1){
+                td = [];
+                colCount = 0;
+            } else if (i == this.props.model.length - 1) {
                 tr.push(<TrWrapper key={'tr-'+tr.length+1}>{td}</TrWrapper>);
             }
         }
@@ -104,4 +123,4 @@ const BaseTable = React.createClass({
         </div>
     }
 })
-module.exports= baseTable;
+module.exports = baseTable;
