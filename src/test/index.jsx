a1acb0556f05ad5cff0e971cf1f561463a3731cb
mervyn.chung <mervyn.chung@foxmail.com>
2016-03-30 15:47:19 +0800

    20160330

--- src/test/index.jsx
+++ src/test/index.jsx
@@ -16,18 +16,38 @@ const data = {
         zgzh: '34454394',
         barq: '2016-01-01'
     },
-    groupNum: 1,
-    keys: {
+    colGroupNum: 2,
+    keys: [{
+        id: 'name',
         name: '姓名',
-        age: '年龄',
-        sex: '性别',
-        xl: '学历',
-        cs: '城市',
-        dz: '地址',
-        byyx: '毕业院校',
-        zgzh: '证书编号',
-        barq: '备案日期'
-    }
+    }, {
+        id: 'age',
+        name: '年龄',
+    }, {
+        id: 'sex',
+        name: '性别'
+    }, {
+        id: 'xl',
+        name: '学历',
+        groupspan: '2'
+    }, {
+        id: 'cs',
+        name: '城市'
+    }, {
+        id: 'dz',
+        name: '地址'
+    }, {
+        id: 'byyx',
+        name: '毕业院校',
+        groupspan: '2'
+    }, {
+        id: 'zgzh',
+        name: '证书编号',
+        groupspan: '2'
+    }, {
+        id: 'barq',
+        name: '备案日期'
+    }]
 }
 const TrWrapper = React.createClass({
     render(){
@@ -37,24 +57,39 @@ const TrWrapper = React.createClass({
 
 const BaseTable = React.createClass({
     render(){
+        let colCount = 0;
         const colgroup = [];
         const tr = [];
         let td = [];
-        const groupNum = this.props.data.groupNum;
+        const colGroupNum = this.props.data.colGroupNum;
         //设置colgroup样式
-        for (let i = 0; i < (groupNum < 3 ? groupNum : 2); i++) {
-            var spanKey = 24 / (groupNum * 3);
-            var spanValue = 24 * 2 / (groupNum * 3);
+        for (let i = 0; i < (colGroupNum < 3 ? colGroupNum : 2); i++) {
+            let spanKey = 24 / (colGroupNum * 3);
+            let spanValue = 24 * 2 / (colGroupNum * 3);
             colgroup.push(<col key={'c-k-'+i} className={'col-'+spanKey}></col>);
             colgroup.push(<col key={'c-v-'+i} className={'col-'+spanValue}></col>);
         }
-        //将实体内容以key:value放置到对应的td组中，再按照groupNum分列
-        for (let prop in this.props.data.keys) {
-            td.push(<td key={'td-k-'+prop}>{this.props.data.keys[prop]}</td>);
-            td.push(<td key={'td-v-'+prop}>{this.props.data.values[prop]}</td>);
-            if (td.length == groupNum * 2) {
+        //将实体内容以key:value放置到对应的td组中，再按照colGroupNum分列
+        for (let i = 0; i < this.props.data.keys.length; i++) {
+            let key = this.props.data.keys[i];
+            if(key.groupspan){
+                if(colGroupNum < colCount+key.groupspan ){
+                    tr.push(<TrWrapper key={'tr-'+tr.length+1}>{td}</TrWrapper>);
+                    td=[];colCount=0;
+                }
+                td.push(<td key={'td-k-'+key.id}>{key.name}</td>);
+                td.push(<td key={'td-v-'+key.id} colSpan={key.groupspan*2-1}>{this.props.data.values[key.id]}</td>);
+                colCount+=key.groupspan
+            }else{
+                td.push(<td key={'td-k-'+key.id}>{key.name}</td>);
+                td.push(<td key={'td-v-'+key.id}>{this.props.data.values[key.id]}</td>);
+                colCount+=1;
+            }
+            if (0 == colCount % colGroupNum ) {
                 tr.push(<TrWrapper key={'tr-'+tr.length+1}>{td}</TrWrapper>);
-                td = []
+                td = [];colCount=0;
+            }else if(i==this.props.data.keys.length-1){
+                tr.push(<TrWrapper key={'tr-'+tr.length+1}>{td}</TrWrapper>);
             }
         }
         return <div className={'base-table '+
