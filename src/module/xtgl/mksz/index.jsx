3a82043dfb46a093fb2a374e955b5ff190c4cbdc
mervyn.chung <mervyn.chung@foxmail.com>
2016-04-12 00:55:50 +0800

    #105

--- src/module/xtgl/mksz/index.jsx
+++ src/module/xtgl/mksz/index.jsx
@@ -1,7 +1,7 @@
-/*模块设置功能*/
+/*中心端模块设置功能*/
 module.exports = {
-    path: 'xtgl/mksz(/)',
-    breadcrumbName:'模块设置',
+    path: 'xtgl/zxdmksz(/)',
+    breadcrumbName:'中心端模块设置',
     getComponent(location, cb) {
         require.ensure([], (require) => {
             cb(null, require('./component'))
