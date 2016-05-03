8d2ade5d38b33ada11fd09239b83aca14ebaf4ae
Fsenxu <fasen.xu@gdkyit.com>
2016-05-03 10:48:24 +0800

    update from master

--- src/entry/index.jsx
+++ src/entry/index.jsx
@@ -13,23 +13,37 @@ const routes = {
     ignoreScrollBehavior: true,
     breadcrumbName: '首页',
     childRoutes: [
-        //模块预加载方式
+        /* 模块预加载方式 */
         //{ path: '/gn1(/)', component: gn1 },
 
-        //模块懒加载方式
-        require('../module/jggl/swscx'),
-        require('../module/rygl/rycx'),
+
+        /* 模块懒加载方式 */
+        //机构管理
+       require('../module/jggl/swscx'),
+
+        //人员管理
+       require('../module/rygl/rycx'),
+
+        //系统功能设置
         require('../module/xtgnsz/mkgl'),
+
+        //业务管理
         require('../module/ywgl/ywbbgl'),
         require('../module/ywgl/ywzlgl/sfjeyjgl'),
         require('../module/ywgl/ywzlgl/ndbtyjgl'),
         require('../module/ywgl/ywzlgl/ywwtyjgl'),
         require('../module/ywgl/ywzlgl/cxbgyjgl'),
+
+        //会员会费管理
         require('../module/hyhfgl/hfjlqk'),
         require('../module/hyhfgl/grhyhfgl'),
         require('../module/hyhfgl/fzyhyhfgl'),
+
+        //财务报表
         require('../module/cwbb/lrfpb'),
-        require('../module/cwbb/zcmxb')
+        require('../module/cwbb/zcmxb'),
+        require('../module/cwbb/lrb'),
+        require('../module/cwbb/zcfzb')
     ]
 };
 
