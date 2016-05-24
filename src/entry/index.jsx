49e30d6bb7f57633c31233712c2ebe83291cdb40
mervyn.chung <mervyn.chung@foxmail.com>
2016-05-24 17:14:00 +0800

    #28

--- src/entry/index.jsx
+++ src/entry/index.jsx
@@ -43,64 +43,62 @@ const Index = React.createClass({
 
                 /* 模块懒加载方式 */
                 //机构管理
-                require('../module/xtgnsz/mkgl'),
                 require('../module/jggl/swscx'),
-                require('../module/cwbb/lrb')
 
                 //人员管理
-                /*       require('../module/rygl/rycx'),
+                require('../module/rygl/rycx'),
 
-                 //系统功能设置
-                 require('../module/xtgnsz/mkgl'),
+                //系统功能设置
+                require('../module/xtgnsz/mkgl'),
 
-                 //业务管理
-                 require('../module/ywgl/ywbbgl'),
-                 require('../module/ywgl/ywzlgl/sfjeyjgl'),
-                 require('../module/ywgl/ywzlgl/ndbtyjgl'),
-                 require('../module/ywgl/ywzlgl/ywwtyjgl'),
-                 require('../module/ywgl/ywzlgl/cxbgyjgl'),
+                //业务管理
+                require('../module/ywgl/ywbbgl'),
+                require('../module/ywgl/ywzlgl/sfjeyjgl'),
+                require('../module/ywgl/ywzlgl/ndbtyjgl'),
+                require('../module/ywgl/ywzlgl/ywwtyjgl'),
+                require('../module/ywgl/ywzlgl/cxbgyjgl'),
 
-                 //会员会费管理
-                 require('../module/hyhfgl/hfjlqk'),
-                 require('../module/hyhfgl/grhyhfgl'),
-                 require('../module/hyhfgl/fzyhyhfgl'),
+                //会员会费管理
+                require('../module/hyhfgl/hfjlqk'),
+                require('../module/hyhfgl/grhyhfgl'),
+                require('../module/hyhfgl/fzyhyhfgl'),
 
-                 //财务报表
-                 require('../module/cwbb/lrfpb'),
-                 require('../module/cwbb/xjllb'),
-                 require('../module/cwbb/zcmxb'),
-                 require('../module/cwbb/lrb'),
-                 require('../module/cwbb/zcfzb'),
-                 require('../module/cwbb/wsbbb'),
-
-
-                 //手动上报报表
-                 require('../module/sdsbbb/jzywtjb'),
-                 require('../module/sdsbbb/jygmtjb'),
-                 require('../module/sdsbbb/swsjbqkb'),
-                 require('../module/sdsbbb/hyryqktj'),
-                 require('../module/sdsbbb/jysrqktj'),
-                 require('../module/sdsbbb/wsbbbcx'),
+                //财务报表
+                require('../module/cwbb/lrfpb'),
+                require('../module/cwbb/xjllb'),
+                require('../module/cwbb/zcmxb'),
+                require('../module/cwbb/lrb'),
+                require('../module/cwbb/zcfzb'),
+                require('../module/cwbb/wsbbb'),
 
 
-                 //历史记录查询
-                 require('../module/lsjlcx/swslsjl/bglsjl'),
-                 require('../module/lsjlcx/swslsjl/yhbsws'),
-                 require('../module/lsjlcx/swslsjl/yzxsws'),
-                 require('../module/lsjlcx/zyswslsjl/snbgjl'),
-                 require('../module/lsjlcx/zyswslsjl/snzsjl'),
-                 require('../module/lsjlcx/zyswslsjl/snzzfjl'),
-                 require('../module/lsjlcx/zyswslsjl/snzjjl'),
-                 require('../module/lsjlcx/zyswslsjl/snzxjl'),
-                 require('../module/lsjlcx/zyswslsjl/snbdjl'),
-                 require('../module/lsjlcx/fzyzjjl'),
-                 require('../module/lsjlcx/fzyzxjl'),
-                 require('../module/lsjlcx/fzyzzyjl'),
+                //手动上报报表
+                require('../module/sdsbbb/jzywtjb'),
+                require('../module/sdsbbb/jygmtjb'),
+                require('../module/sdsbbb/swsjbqkb'),
+                require('../module/sdsbbb/hyryqktj'),
+                require('../module/sdsbbb/jysrqktj'),
+                require('../module/sdsbbb/wsbbbcx'),
 
 
-                 //监督检查
-                 require('../module/jdjc/zyswsnjb'),
-                 require('../module/jdjc/swsnj')*/
+                //历史记录查询
+                require('../module/lsjlcx/swslsjl/bglsjl'),
+                require('../module/lsjlcx/swslsjl/yhbsws'),
+                require('../module/lsjlcx/swslsjl/yzxsws'),
+                require('../module/lsjlcx/zyswslsjl/snbgjl'),
+                require('../module/lsjlcx/zyswslsjl/snzsjl'),
+                require('../module/lsjlcx/zyswslsjl/snzzfjl'),
+                require('../module/lsjlcx/zyswslsjl/snzjjl'),
+                require('../module/lsjlcx/zyswslsjl/snzxjl'),
+                require('../module/lsjlcx/zyswslsjl/snbdjl'),
+                require('../module/lsjlcx/fzyzjjl'),
+                require('../module/lsjlcx/fzyzxjl'),
+                require('../module/lsjlcx/fzyzzyjl'),
+
+
+                //监督检查
+                require('../module/jdjc/zyswsnjb'),
+                require('../module/jdjc/swsnj')
             ]
         }, {
             path: '/signin',
