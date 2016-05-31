accd548969f7c68fc80148b7443885869e1fe95a
mervyn.chung <mervyn.chung@foxmail.com>
2016-05-31 16:24:22 +0800

    #1

---
+++ src/module/xtgnsz/qxgl/component.jsx
@@ -0,0 +1,18 @@
+import './style.css'
+import React from 'react'
+import {Tabs} from 'antd'
+import Panel from 'component/compPanel'
+
+
+const TabPane = Tabs.TabPane;
+
+//模块设置
+const qxgl = React.createClass({
+    render(){
+        return <div className="qxgl">
+
+        </div>
+    }
+})
+
+module.exports = qxgl;
