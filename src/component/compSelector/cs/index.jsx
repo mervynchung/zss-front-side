46877377cabe1a2194270b6f374de6f8a3d41d99
tenrock6873155 <1076576310@qq.com>
2016-06-23 16:57:48 +0800

    #6-23

--- src/component/compSelector/cs/index.jsx
+++ src/component/compSelector/cs/index.jsx
@@ -9,7 +9,7 @@ const Option = Select.Option;
 
 const selectorCS = React.createClass({
     render(){
-        const options = data.map(item=><Option key={item.id}>{item.mc}</Option>)
+        const options = data.map(item=><Option key={item.id} value={item.id}>{item.mc}</Option>)
         return <Select {...this.props} placeholder="选择城市"  >
             {options}
         </Select>
