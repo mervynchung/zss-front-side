8557bd57ae0fb26f08be9abbe2c22ad19d60d4c1
mervyn.chung <mervyn.chung@foxmail.com>
2016-06-29 20:15:52 +0800

    #12

--- src/module/xtgnsz/yhgl/component.jsx
+++ src/module/xtgnsz/yhgl/component.jsx
@@ -1,14 +1,16 @@
 import './style.css'
 import React from 'react'
-import {Table,Col,Row,Tree,Tabs,Modal,Button,Spin,notification } from 'antd'
+import {Table,Col,Row,Tree,Tabs,Modal,Button,Spin,notification,Icon } from 'antd'
 import Panel from 'component/compPanel'
 import config from 'common/configuration'
 import {SelectorRoles,SelectorDQ,SelectorCS} from 'component/compSelector'
+import SearchForm from './searchForm'
 import model from './model.jsx'
 import req from 'reqwest'
 import {jsonCopy} from 'common/utils.js'
 
 const ToolBar = Panel.ToolBar;
+const ButtonGroup = Button.Group;
 const USER_URL = config.HOST + config.URI_API_FRAMEWORK + '/users';
 const ROLE_URL = config.HOST + config.URI_API_FRAMEWORK + '/roles';
 
@@ -42,10 +44,13 @@ const yhgl = React.createClass({
     getInitialState(){
         return {
             pageLoading: true,
+            searchToggle:false,
+            helper:false,
             roles: [],
             users: [],
             select: '',
-            where:'',
+            where: '',
+            selectedRowKeys: [],
             pagination: {
                 current: 1,
                 showSizeChanger: true,
@@ -75,58 +80,101 @@ const yhgl = React.createClass({
             Modal.error({
                 title: '数据获取错误',
                 content: (
-                  <div>
-                      <p>无法从服务器返回数据，需检查应用服务工作情况</p>
-                      <p>Status: {e.status}</p>
-                  </div>  )
+                    <div>
+                        <p>无法从服务器返回数据，需检查应用服务工作情况</p>
+                        <p>Status: {e.status}</p>
+                    </div>  )
             });
         })
     },
-    handlePageChange(pagination){
-        const pager = this.state.pagination;
-        pager.current = pagination.current;
-        pager.pageSize = pagination.pageSize;
-        this.setState({pagination: pager});
 
+    //数据表换页
+    handlePageChange(pager){
         fetchUsers({
             page: pager.current,
             pageSize: pager.pageSize,
             where: encodeURIComponent(JSON.stringify(this.state.where))
-        }).then(resp=>{
-            const pager = this.state.pagination;
-            pager.current = pagination.current;
-            pager.pageSize = pagination.pageSize;
-            this.setState({pagination: pager,users:resp.data});
+        }).then(resp=> {
+            pager.total = resp.total > 1000 ? 1000 : resp.total;
+            pager.showTotal = total => {
+                return `共 ${resp.total} 条，显示前 ${total} 条`
+            };
+            this.setState({pagination: pager, users: resp.data, selectedRowKeys: []});
         })
     },
+    //选择角色
     handleRoleSelected(value){
         const pager = this.state.pagination;
-        pager.current = 1;
-        let where = {roleId:value};
+        let where = {roleId: value};
         const param = {
-            page:1,
-            pageSize:pager.pageSize,
+            page: 1,
+            pageSize: pager.pageSize,
             where: JSON.stringify(where)
         };
-        fetchUsers(param).then(resp=>{
-            this.setState({users:resp.data,where:where,pagination:pager});
+        fetchUsers(param).then(resp=> {
+            pager.total = resp.total > 1000 ? 1000 : resp.total;
+            pager.showTotal = total => {
+                return `共 ${resp.total} 条，显示前 ${total} 条`
+            };
+            pager.current = 1;
+            this.setState({users: resp.data, where: where, pagination: pager});
         })
+    },
+    //表格中的复选框勾选
+    handleSelectedRowChange(selectedRowKeys, selectedRows){
+        this.setState({selectedRowKeys: selectedRowKeys})
+    },
+    //打开关闭查询框
+    handleSearchToggle(){
+        this.setState({searchToggle: !this.state.searchToggle})
+    },
+    //刷新数据
+    handleRefresh(){
+        const pager = this.state.pagination;
+        this.setState({pagination: pager, where: ''});
+        fetchUsers().then(resp=> {
+            pager.total = resp.total > 1000 ? 1000 : resp.total;
+            pager.showTotal = total => {
+                return `共 ${resp.total} 条，显示前 ${total} 条`
+            };
+            pager.current = 1;
+            this.setState({users: resp.data, where:'', pagination: pager});
+        });
+    },
+
+    //帮助信息
+    handleHelper(){
+        this.setState({helper: !this.state.helper})
+    },
+    //查询提交
+    handleSearchSubmit(values){
+
     },
 
 
     render(){
         const rowSelection = {
             type: 'checkbox',
-            selectedRowKeys: [this.state.currentIndex],
-            onSelect: this.handleSelect
+            selectedRowKeys: this.state.selectedRowKeys,
+            onChange: this.handleSelectedRowChange
         };
 
         const toolbar = <ToolBar>
+            <Button onClick={this.handleSearchToggle}>
+                <Icon type="search"/>查询
+                { this.state.searchToggle ? <Icon className="toggle-tip" type="circle-o-up"/> :
+                    <Icon className="toggle-tip" type="circle-o-down"/>}
+            </Button>
+
+            <ButtonGroup>
+                <Button type="primary" onClick={this.handleHelper}><Icon type="question"/></Button>
+                <Button type="primary" onClick={this.handleRefresh}><Icon type="reload"/></Button>
+            </ButtonGroup>
             <SelectorRoles
                 style={{width:'180px'}}
                 data={this.state.roles}
                 optionFilterProp="children"
-                onChange = {this.handleRoleSelected}/>
+                onChange={this.handleRoleSelected}/>
         </ToolBar>;
 
 
@@ -134,7 +182,8 @@ const yhgl = React.createClass({
             <div className="wrap">
                 <Spin spinning={this.state.pageLoading}>
                     <Panel title="用户管理" toolbar={toolbar}>
-
+                        {this.state.searchToggle && <SearchForm
+                            onSubmit={this.handleSearchSubmit}/>}
                         <Table className="outer-border"
                                columns={model.columns}
                                dataSource={this.state.users}
