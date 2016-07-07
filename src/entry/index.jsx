7793636f9bf82086916e3f2d1e0df84a137d46ff
mervyn.chung <mervyn.chung@foxmail.com>
2016-07-07 15:21:49 +0800

    070701

--- src/entry/index.jsx
+++ src/entry/index.jsx
@@ -58,8 +58,8 @@ const Index = React.createClass({
                 require('../module/xtgnsz/qxgl'),
                 require('../module/xtgnsz/yhgl'),
 
-            //业务管理
-                require('../module/ywgl/ywbbgl'),
+                //业务管理
+               /* require('../module/ywgl/ywbbgl'),
                 require('../module/ywgl/ywzlgl/sfjeyjgl'),
                 require('../module/ywgl/ywzlgl/ndbtyjgl'),
                 require('../module/ywgl/ywzlgl/ywwtyjgl'),
@@ -132,7 +132,7 @@ const Index = React.createClass({
                 // //手动上报报表
                 require('../module/client_1/swsqkb'),
                 require('../module/client_1/jygmtjb'),
-                require('../module/client_1/jzywqktjb'),
+                require('../module/client_1/jzywqktjb'),*/
 
                 //业务管理
                 require('../module/client/ywgl/khxxgl') //客户信息管理
