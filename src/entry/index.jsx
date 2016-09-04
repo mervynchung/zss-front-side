0282a944cfc9a5a0958aaf9005ce31273eb6a747
mervyn.chung <mervyn.chung@foxmail.com>
2016-09-04 17:55:37 +0800

    index fix

--- src/entry/index.jsx
+++ src/entry/index.jsx
@@ -131,7 +131,7 @@ const Index = React.createClass({
                 require('../module/spsh/module/39'),
                 require('../module/spsh/module/43'),
                 require('../module/spsh/module/44'),
-                require('../module/spsh/module/46'),/*
+                require('../module/spsh/module/46'),
 
                 //数据统计分析 - 年检数据分析
                 require('../module/xtsjfx/njsjfx/rynjsjfx'),//人员年检数据分析
