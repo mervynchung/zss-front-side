f842f6de15103691f57760b26eb2f09647e59c99
bubleSun <1014077044@qq.com>
2016-09-29 11:22:32 +0800

    #092901

--- src/entry/index.jsx
+++ src/entry/index.jsx
@@ -12,7 +12,7 @@ import Dyzb from 'module/useforprint/glsc_zb/dy.jsx';
 import Dyfb from 'module/useforprint/glsc_fb/dy.jsx';
 import Zyjbxx from 'module/rygl/rycx/zyryjbxx.jsx';
 import Cyjbxx from 'module/rygl/rycx/cyryjbxx.jsx';
-import {cenHome,clientHome } from '../module/home';
+import {cenHome, clientHome } from '../module/home';
 import NotFound from 'module/404notfound'
 import auth from 'common/auth'
 
@@ -24,7 +24,7 @@ const history = useRouterHistory(createHashHistory)({
 const Index = React.createClass({
 
     /*登录校验*/
-    requireAuth(nextState, replace){
+    requireAuth(nextState, replace) {
         /*if (!auth.verifyPermission(nextState.location.pathname)) {
          replace({
          pathname: '/404',
@@ -34,17 +34,17 @@ const Index = React.createClass({
         if (!auth.verifyAuth()) {
             replace({
                 pathname: '/signin',
-                state: {nextPathname: nextState.location.pathname}
+                state: { nextPathname: nextState.location.pathname }
             })
         }
     },
 
-    render(){
+    render() {
         /*路由配置*/
         const routes = [{
             path: '/',
             component: App,
-            indexRoute: {component: auth.isClient() ? clientHome : cenHome},
+            indexRoute: { component: auth.isClient() ? clientHome : cenHome },
             ignoreScrollBehavior: true,
             breadcrumbName: '首页',
             onEnter: this.requireAuth,
@@ -59,7 +59,7 @@ const Index = React.createClass({
                 require('../module/jggl/swsslsp'),
 
                 //人员管理
-              //  require('../module/rygl/rycx'),
+                //  require('../module/rygl/rycx'),
 
                 //系统功能设置
                 require('../module/xtgnsz/mkgl'),
@@ -71,121 +71,121 @@ const Index = React.createClass({
                 require('../module/zzgl/jgzzgl'), //事务所资质管理
                 require('../module/zzgl/zyswszzgl'), //执业注师资质管理
 
+                /*
+                                //业务管理
+                                require('../module/ywgl/ywbbgl'),
+                                require('../module/ywgl/ywbbthsp'), //业务报备退回审批
+                                require('../module/ywgl/ywbbcxsp'), //业务报备退回审批
+                                require('../module/ywgl/ywbbqysp'), //业务报备退回审批
+                                require('../module/ywgl/sfjeyjgl'), //收费金额预警管理
+                                require('../module/ywgl/ndbtyjgl'), //年度不同预警管理
+                                require('../module/ywgl/ywwtfyjgl'), //业务委托方预警管理
+                                require('../module/ywgl/setting'), //业务参数设置
+                                require('../module/ywgl/ywbbsjfx'),//业务报备数据分析
+                
+                
+                
+                
+                                 
+                                //财务报表
+                
+                                require('../module/cwbb/lrfpb'),
+                                require('../module/cwbb/xjllb'),
+                                require('../module/cwbb/zcmxb'),
+                                require('../module/cwbb/lrb'),
+                                require('../module/cwbb/zcfzb'),
+                                require('../module/cwbb/wsbbb'),
+                
+                                //手动上报报表
+                                require('../module/sdsbbb/jzywtjb'),
+                                require('../module/sdsbbb/jygmtjb'),
+                                require('../module/sdsbbb/swsjbqkb'),
+                                require('../module/sdsbbb/hyryqktj'),
+                                require('../module/sdsbbb/jysrqktj'),
+                                require('../module/sdsbbb/wsbbbcx'),
+                
+                                //历史记录查询
+                                require('../module/lsjlcx/swslsjl/bglsjl'),
+                                require('../module/lsjlcx/swslsjl/yhbsws'),
+                                require('../module/lsjlcx/swslsjl/yzxsws'),
+                                require('../module/lsjlcx/zyswslsjl/snbgjl'),
+                                require('../module/lsjlcx/zyswslsjl/snzsjl'),
+                                require('../module/lsjlcx/zyswslsjl/snzzfjl'),
+                                require('../module/lsjlcx/zyswslsjl/snzjjl'),
+                                require('../module/lsjlcx/zyswslsjl/snzxjl'),
+                                require('../module/lsjlcx/zyswslsjl/snbdjl'),
+                                require('../module/lsjlcx/fzyzjjl'),
+                                require('../module/lsjlcx/fzyzxjl'),
+                                require('../module/lsjlcx/fzyzzyjl'),
+                
+                                //监督检查
+                                require('../module/jdjc/zyswsnjb'),
+                                require('../module/jdjc/swsnj'),
+                
+                
+                                //逐级审核
+                
+                                require('../module/spsh'),
+                                require('../module/spshlsjl'),
+                                require('../module/spsh/module/1'),
+                                require('../module/spsh/module/2'),
+                                require('../module/spsh/module/3'),
+                                require('../module/spsh/module/4'),
+                                require('../module/spsh/module/5'),
+                                require('../module/spsh/module/6'),
+                                require('../module/spsh/module/7'),
+                                require('../module/spsh/module/8'),
+                                require('../module/spsh/module/9'),
+                                require('../module/spsh/module/10'),
+                                require('../module/spsh/module/11'),
+                                require('../module/spsh/module/12'),
+                                require('../module/spsh/module/13'),
+                                require('../module/spsh/module/14'),
+                                require('../module/spsh/module/15'),
+                                require('../module/spsh/module/18'),
+                                require('../module/spsh/module/20'),
+                                require('../module/spsh/module/38'),
+                                require('../module/spsh/module/39'),
+                                require('../module/spsh/module/43'),
+                                require('../module/spsh/module/44'),
+                                require('../module/spsh/module/46'),
+                
+                                //数据统计分析 - 年检数据分析
+                                require('../module/xtsjfx/njsjfx/rynjsjfx'),//人员年检数据分析
+                                require('../module/xtsjfx/njsjfx/jgnjsjfxb'),//机构年检数据分析
+                
+                               //数据统计分析 - 注税行业年报表
+                                require('../module/xtsjfx/zshynbb/hyjygmqktj'),//行业经营规模情况统计
+                                require('../module/xtsjfx/zshynbb/swsjbqktjb'),//事务所基本情况统计表1
+                                require('../module/xtsjfx/zshynbb/hyryqktjb'),//行业人员情况统计表2
+                                require('../module/xtsjfx/zshynbb/swsjgqktjb'),//事务所机构情况统计表3
+                                require('../module/xtsjfx/zshynbb/hyjysrqkhzb'),//行业经营收入情况汇总表4
+                                require('../module/xtsjfx/zshynbb/hyjygmqktjb'),//行业经营规模情况统计表5
+                                require('../module/xtsjfx/zshynbb/hyjzywqktjb'),//行业鉴证业务情况统计表6
+                
+                
+                               //数据统计分析 - 系统统计报表
+                                require('../module/xtsjfx/xttjbb/swsqktjA'), //事务所情况统计A
+                                require('../module/xtsjfx/xttjbb/hyryqktj'), //行业人员情况统计
+                                require('../module/xtsjfx/xttjbb/swsqktj_b'),//事务所情况统计B
+                                require('../module/xtsjfx/xttjbb/swszttj'),//税务师状态统计 
+                                require('../module/xtsjfx/xttjbb/zyswszjtj'),//税务师转籍统计
+                                require('../module/xtsjfx/xttjbb/zyswszctj'),//税务师转出统计
+                
+                                //数据统计分析 - 系统业务报表
+                                require('../module/xtsjfx/xtywbb/ndjysrtj'), //年度经营收入统计 
+                                require('../module/xtsjfx/xtywbb/ndjzqktj'), //年度鉴证情况统计
+                
+                                //数据统计分析 - 数据分析
+                                require('../module/xtsjfx/sjfx/zyzzsjfx'),//执业资质数据分析
+                                require('../module/xtsjfx/sjfx/hyxlsjfx'),//行业学历数据分析
+                                require('../module/xtsjfx/sjfx/zjgmsjfx'),//资金规模数据分析
+                                require('../module/xtsjfx/sjfx/hynlsjfx'),//行业年龄数据分析
+                                require('../module/xtsjfx/sjfx/ryztsjfx'),//人员状态数据分析
+                                require('../module/xtsjfx/sjfx/zyzshsjfx'),*/
 
-                //业务管理
-                require('../module/ywgl/ywbbgl'),
-                require('../module/ywgl/ywbbthsp'), //业务报备退回审批
-                require('../module/ywgl/ywbbcxsp'), //业务报备退回审批
-                require('../module/ywgl/ywbbqysp'), //业务报备退回审批
-                require('../module/ywgl/sfjeyjgl'), //收费金额预警管理
-                require('../module/ywgl/ndbtyjgl'), //年度不同预警管理
-                require('../module/ywgl/ywwtfyjgl'), //业务委托方预警管理
-                require('../module/ywgl/setting'), //业务参数设置
-                require('../module/ywgl/ywbbsjfx'),//业务报备数据分析
-
-
-
-
-                 
-                //财务报表
-
-                require('../module/cwbb/lrfpb'),
-                require('../module/cwbb/xjllb'),
-                require('../module/cwbb/zcmxb'),
-                require('../module/cwbb/lrb'),
-                require('../module/cwbb/zcfzb'),
-                require('../module/cwbb/wsbbb'),
-
-                //手动上报报表
-                require('../module/sdsbbb/jzywtjb'),
-                require('../module/sdsbbb/jygmtjb'),
-                require('../module/sdsbbb/swsjbqkb'),
-                require('../module/sdsbbb/hyryqktj'),
-                require('../module/sdsbbb/jysrqktj'),
-                require('../module/sdsbbb/wsbbbcx'),
-
-                //历史记录查询
-                require('../module/lsjlcx/swslsjl/bglsjl'),
-                require('../module/lsjlcx/swslsjl/yhbsws'),
-                require('../module/lsjlcx/swslsjl/yzxsws'),
-                require('../module/lsjlcx/zyswslsjl/snbgjl'),
-                require('../module/lsjlcx/zyswslsjl/snzsjl'),
-                require('../module/lsjlcx/zyswslsjl/snzzfjl'),
-                require('../module/lsjlcx/zyswslsjl/snzjjl'),
-                require('../module/lsjlcx/zyswslsjl/snzxjl'),
-                require('../module/lsjlcx/zyswslsjl/snbdjl'),
-                require('../module/lsjlcx/fzyzjjl'),
-                require('../module/lsjlcx/fzyzxjl'),
-                require('../module/lsjlcx/fzyzzyjl'),
-
-                //监督检查
-                require('../module/jdjc/zyswsnjb'),
-                require('../module/jdjc/swsnj'),
-
-
-                //逐级审核
-
-                require('../module/spsh'),
-                require('../module/spshlsjl'),
-                require('../module/spsh/module/1'),
-                require('../module/spsh/module/2'),
-                require('../module/spsh/module/3'),
-                require('../module/spsh/module/4'),
-                require('../module/spsh/module/5'),
-                require('../module/spsh/module/6'),
-                require('../module/spsh/module/7'),
-                require('../module/spsh/module/8'),
-                require('../module/spsh/module/9'),
-                require('../module/spsh/module/10'),
-                require('../module/spsh/module/11'),
-                require('../module/spsh/module/12'),
-                require('../module/spsh/module/13'),
-                require('../module/spsh/module/14'),
-                require('../module/spsh/module/15'),
-                require('../module/spsh/module/18'),
-                require('../module/spsh/module/20'),
-                require('../module/spsh/module/38'),
-                require('../module/spsh/module/39'),
-                require('../module/spsh/module/43'),
-                require('../module/spsh/module/44'),
-                require('../module/spsh/module/46'),
-
-                //数据统计分析 - 年检数据分析
-                require('../module/xtsjfx/njsjfx/rynjsjfx'),//人员年检数据分析
-                require('../module/xtsjfx/njsjfx/jgnjsjfxb'),//机构年检数据分析
-
-               //数据统计分析 - 注税行业年报表
-                require('../module/xtsjfx/zshynbb/hyjygmqktj'),//行业经营规模情况统计
-                require('../module/xtsjfx/zshynbb/swsjbqktjb'),//事务所基本情况统计表1
-                require('../module/xtsjfx/zshynbb/hyryqktjb'),//行业人员情况统计表2
-                require('../module/xtsjfx/zshynbb/swsjgqktjb'),//事务所机构情况统计表3
-                require('../module/xtsjfx/zshynbb/hyjysrqkhzb'),//行业经营收入情况汇总表4
-                require('../module/xtsjfx/zshynbb/hyjygmqktjb'),//行业经营规模情况统计表5
-                require('../module/xtsjfx/zshynbb/hyjzywqktjb'),//行业鉴证业务情况统计表6
-
-
-               //数据统计分析 - 系统统计报表
-                require('../module/xtsjfx/xttjbb/swsqktjA'), //事务所情况统计A
-                require('../module/xtsjfx/xttjbb/hyryqktj'), //行业人员情况统计
-                require('../module/xtsjfx/xttjbb/swsqktj_b'),//事务所情况统计B
-                require('../module/xtsjfx/xttjbb/swszttj'),//税务师状态统计 
-                require('../module/xtsjfx/xttjbb/zyswszjtj'),//税务师转籍统计
-                require('../module/xtsjfx/xttjbb/zyswszctj'),//税务师转出统计
-
-                //数据统计分析 - 系统业务报表
-                require('../module/xtsjfx/xtywbb/ndjysrtj'), //年度经营收入统计 
-                require('../module/xtsjfx/xtywbb/ndjzqktj'), //年度鉴证情况统计
-
-                //数据统计分析 - 数据分析
-                require('../module/xtsjfx/sjfx/zyzzsjfx'),//执业资质数据分析
-                require('../module/xtsjfx/sjfx/hyxlsjfx'),//行业学历数据分析
-                require('../module/xtsjfx/sjfx/zjgmsjfx'),//资金规模数据分析
-                require('../module/xtsjfx/sjfx/hynlsjfx'),//行业年龄数据分析
-                require('../module/xtsjfx/sjfx/ryztsjfx'),//人员状态数据分析
-                require('../module/xtsjfx/sjfx/zyzshsjfx'),
-
-        
-
+                //用户登录记录表
+                require('../module/yhdljlb'),
 
                 //会员会费缴纳
                 require('../module/hyhf_new/hyhfjnqk'),
@@ -195,6 +195,8 @@ const Index = React.createClass({
 
                 //证照打印管理
                 require('../module/zzdygl/zyglscdy'),
+
+
 
                 //客户端
                 //机构管理
@@ -211,24 +213,24 @@ const Index = React.createClass({
                 require('../module/client/swsrygl/zyrygl'),
                 require('../module/client/swsrygl/rydr'),
 
-               //财务报表上传
+                //财务报表上传
                 require('../module/client/cwbb/lrb'),
                 require('../module/client/cwbb/lrfpb'),
                 require('../module/client/cwbb/xjllb'),
                 require('../module/client/cwbb/zcfzb'),
                 require('../module/client/cwbb/zcmxb'),
 
-               //手动上报报表
+                //手动上报报表
                 require('../module/client/sdsb/swsqkb'),
                 require('../module/client/sdsb/jygmtjb'),
                 require('../module/client/sdsb/jzywqktjb'),
                 require('../module/client/sdsb/hyryqktjb'),
                 require('../module/client/sdsb/jysrqkb'),
 
-               //年度检查
-               require('../module/client/swsnj'),//事务所年检
-               require('../module/client/zyswsnj'),//执业税务师年检
-               
+                //年度检查
+                require('../module/client/swsnj'),//事务所年检
+                require('../module/client/zyswsnj'),//执业税务师年检
+
 
                 //业务管理
                 require('../module/client/ywgl/khxxgl'), //客户信息管理
@@ -241,33 +243,33 @@ const Index = React.createClass({
                 require('../module/updatepass')
             ]
         }, {
-            path: '/signin',
-            component: Signin
-        }, {
-            path: '/print/hyhf/fpdy',
-            component: Dyfp
-        }, {
-            path: '/print/zzdygl/glscdj',
-            component: Dydj
-        }, {
-            path: '/print/zzdygl/glscry',
-            component: Dyry
-        }, {
-            path: '/print/zzdygl/jgzyzzb',
-            component: Dyzb
-        }, {
-            path: '/print/zzdygl/jgzyzfb',
-            component: Dyfb
-        }, {
-            path: '/new_blank/sws/zyjbxx',
-            component: Zyjbxx
-        },{
-            path: '/new_blank/sws/cyjbxx',
-            component: Cyjbxx
-        }, {
-            path: '*',
-            component: NotFound
-        }];
+                path: '/signin',
+                component: Signin
+            }, {
+                path: '/print/hyhf/fpdy',
+                component: Dyfp
+            }, {
+                path: '/print/zzdygl/glscdj',
+                component: Dydj
+            }, {
+                path: '/print/zzdygl/glscry',
+                component: Dyry
+            }, {
+                path: '/print/zzdygl/jgzyzzb',
+                component: Dyzb
+            }, {
+                path: '/print/zzdygl/jgzyzfb',
+                component: Dyfb
+            }, {
+                path: '/new_blank/sws/zyjbxx',
+                component: Zyjbxx
+            }, {
+                path: '/new_blank/sws/cyjbxx',
+                component: Cyjbxx
+            }, {
+                path: '*',
+                component: NotFound
+            }];
         return <Router history={history} routes={routes}/>
     }
 });
---
+++ src/module/yhdljlb/component.jsx
@@ -0,0 +1,200 @@
+/*
+用户登录记录表
+*/
+import React from 'react'
+import {Table, Modal, Row, Col, Button, Icon, Alert, Select, Form} from 'antd'
+import CompPageHead from 'component/CompPageHead'
+import Panel from 'component/compPanel'
+import {columns} from './model'
+import req from 'reqwest';
+import config from 'common/configuration'
+import BaseTable from 'component/compBaseTable'
+import {entityFormat} from 'common/utils'
+import SearchForm from './searchForm'
+
+
+
+const API_URL = config.HOST + config.URI_API_PROJECT + '/yhdljlb';
+const ToolBar = Panel.ToolBar;
+const ButtonGroup = Button.Group;
+
+
+const yhdljlb = React.createClass({
+    getInitialState() {
+        return {
+            data: [],
+            pagination: {
+                current: 1,
+                showSizeChanger: true,
+                pageSize: 5,
+                showQuickJumper: true,
+                pageSizeOptions: ['5', '10', '20'],
+                alert: ''
+
+            },
+            searchToggle: false,
+            detailViewToggle: false,
+            where: '',
+            helper: false,
+            entity: '',
+
+            detailHide: true,
+            add: true,
+            update: true,
+
+        }
+    },
+
+    //改变页码
+    handleChange(pagination, filters, sorter) {
+        const pager = this.state.pagination;
+        pager.current = pagination.current;
+        pager.pageSize = pagination.pageSize;
+        this.setState({ pagination: pager });
+
+        this.fetchData({
+            pagenum: pager.current,
+            pageSize: pager.pageSize,
+            where: encodeURIComponent(JSON.stringify(this.state.where))
+        })
+    },
+
+    //查询按钮
+    handleSearchToggle() {
+        this.setState({ searchToggle: !this.state.searchToggle });
+    },
+
+    //刷新按钮
+    handleRefresh() {
+        const pager = this.state.pagination;
+        pager.current = 1;
+        this.setState({ pagination: pager, where: '' });
+        this.fetchData();
+    },
+
+    //帮助按钮
+    handleHelper() {
+        this.setState({ helper: !this.state.helper })
+    },
+    //手动关闭帮助提示
+    handleHelperClose() {
+        this.setState({ helper: false })
+    },
+
+    //提交条件查询
+    handleSearchSubmit(value) {
+        const pager = this.state.pagination;
+        pager.current = 1;
+        const params = {
+            pagenum: 1,
+            pageSize: pager.pageSize,
+            where: encodeURIComponent(JSON.stringify(value))
+        };
+        this.setState({ pagination: pager, where: value });
+        this.fetchData(params);
+        this.setState({ searchToggle: false })
+    },
+
+    //点击某行
+    handleRowClick(record) {
+        req({
+            url: API_URL + '/' + record.id,
+            type: 'json',
+            method: 'get'
+        }).then(resp => {
+            let entity = entityFormat(resp, entityModel);
+            this.setState({ entity: entity, detailHide: false });
+        }).fail(err => {
+            Modal.error({
+                title: '数据获取错误',
+                content: (
+                    <div>
+                        <p>无法从服务器返回数据，需检查应用服务工作情况</p>
+                        <p>Status: {err.status}</p>
+                    </div>)
+            });
+        })
+    },
+    //明细表关闭
+    handleDetailClose() {
+        this.setState({ detailHide: true })
+    },
+
+
+    //通过API获取数据
+    fetchData(params = {pagenum: 1, pageSize: this.state.pagination.pageSize }) {
+        this.setState({ loading: true });
+        req({
+            url: API_URL,
+            type: 'json',
+            method: 'get',
+            data: params,
+            contentType: 'application/json'
+        }).then(resp => {
+            const p = this.state.pagination;
+            p.total = resp.total > 1000 ? 1000 : resp.total;
+            p.showTotal = total => {
+                return `共 ${resp.total} 条，显示前 ${total} 条`
+            };
+            this.setState({
+                data: resp.data,
+                pagination: p,
+                loading: false
+            })
+        }).fail(err => {
+            this.setState({ loading: false });
+            Modal.error({
+                title: '数据获取错误',
+                content: (
+                    <div>
+                        <p>无法从服务器返回数据，需检查应用服务工作情况</p>
+                        <p>Status: {err.status}</p>
+                    </div>)
+            });
+        })
+    },
+
+    componentDidMount() {
+        this.fetchData();
+    },
+
+
+    render() {
+
+        console.log(this.state.data);
+        //定义工具栏内容
+        let toolbar = <ToolBar>
+            { this.state.add && <Button onClick={this.handleSearchToggle}>
+                <Icon type="search"/>查询
+                { this.state.searchToggle ? <Icon className="toggle-tip" type="circle-o-up"/> :
+                    <Icon className="toggle-tip" type="circle-o-down"/>}
+            </Button>}
+        </ToolBar>
+
+        return <div className="yhdljlb">
+            <div className="wrap">
+
+                <Panel title="用户登录记录表" toolbar={toolbar}>
+
+
+
+                    {this.state.searchToggle && <SearchForm
+                        onSubmit={this.handleSearchSubmit}/>}
+                    <div className="h-scroll-table">
+                        <Table columns={columns}
+                            dataSource={this.state.data}
+                            pagination={this.state.pagination}
+                            loading={this.state.loading}
+                            rowKey={record => record.id}
+                            onChange={this.handleChange}
+
+                            />
+                    </div>
+                </Panel>
+
+            </div>
+        </div>
+    }
+});
+
+module.exports = yhdljlb;
