869f892830984aa250db857efba3def37c159655
Fsenxu <fasen.xu@gdkyit.com>
2016-10-13 16:02:40 +0800

    #1013

--- src/module/client/cwbb/zcmxb/component.jsx
+++ src/module/client/cwbb/zcmxb/component.jsx
@@ -2,26 +2,20 @@ import React from 'react'
 import {Table, Modal, Row, Col, Button, Icon, Alert,message} from 'antd'
 import CompPageHead from 'component/CompPageHead'
 import Panel from 'component/compPanel'
-import {handleRowButton, columns, entityModel} from './model'
+import {entityModel} from './model'
 import req from 'reqwest';
 import SearchForm from './searchForm'
 import Add from './Add'
 import Update from './Update'
 import auth from 'common/auth'
 import config from 'common/configuration'
-import BaseTable from 'component/compBaseTable'
-import {entityFormat} from 'common/utils'
 import DetailBox from './detailbox.jsx'
-
 
 const API_URL = config.HOST + config.URI_API_PROJECT + '/add/zcmxb';
 const URL = config.HOST + config.URI_API_PROJECT + '/addzcmxb';
-
+const URL_C = config.HOST + config.URI_API_PROJECT + '/commont/checiftjbb/zcmxb';
 const ToolBar = Panel.ToolBar;
 const ButtonGroup = Button.Group;
-
-
-
 
 const zcmxb = React.createClass({
     //初始化state
@@ -42,9 +36,11 @@ const zcmxb = React.createClass({
             where: '',
             helper: false,
             entity: '',
-           
+           fileds:{},
             detailHide: true,
             add: true,
+            views:0,
+            viewTitle:'支出明细表',
             update: true,
             
         }
@@ -55,7 +51,7 @@ const zcmxb = React.createClass({
         const pager = this.state.pagination;
         pager.current = pagination.current;
         pager.pageSize = pagination.pageSize;
-        this.setState({ pagination: pager, detailHide: true });
+        this.setState({ pagination: pager});
 
         this.fetchData({
             page: pager.current,
@@ -66,15 +62,14 @@ const zcmxb = React.createClass({
 
     //查询按钮
     handleSearchToggle() {
-        this.setState({ searchToggle: !this.state.searchToggle, detailHide: true ,});
+        this.setState({ searchToggle: !this.state.searchToggle,});
     },
-
 
     //刷新按钮
     handleRefresh() {
         const pager = this.state.pagination;
         pager.current = 1;
-        this.setState({ pagination: pager, where: '', detailHide: true,update:true });
+        this.setState({ pagination: pager, where: '',});
         this.fetchData();
     },
 
@@ -82,40 +77,46 @@ const zcmxb = React.createClass({
     handleHelper() {
         this.setState({ helper: !this.state.helper })
     },
-    //打开添加表
-    handleAdd() {
-        this.setState({ add: !this.state.add, detailHide: true ,update: true})
-       
-    },
-    //打开修改表
-    handleUpdate() {
-        this.setState({update: !this.state.update,detailHide: true});
-        
-    },
-   
-
-    //手动关闭帮助提示
+        //手动关闭帮助提示
     handleHelperClose() {
         this.setState({ helper: false })
     },
 
-    //点击提交
-    handleOk(e) {
-        let vv = e;
-        vv.ztbj = '1';                  
-        this.fetchHandle(vv);
+    handleViewChange(e) {
+        let tl=this.state.viewTitle;
+        switch(e){
+            case 0 :tl="支出明细表";this.setState({fileds:{}}); break;
+            case 1 :tl="添加支出明细表";break;
+            case 2 :tl="支出明细表修改";break;
+            case 3 :tl="支出明细表查看";break;
+        }
+        this.setState({  views: e,viewTitle:tl});
+       
     },
+
+    handleRowClick(record, index){
+        let fs ={};
+        for(var key in record){
+            let newkey = key.toLowerCase();
+            fs[newkey] = record[key];
+        }
+        this.setState({ fileds: fs,entity:record});
+    },
+
         //点击保存
-    handleSubmit(value) {
-        let values = value;
-        values.ztbj = '0';
-        this.fetchHandle(values); 
+    handleSubmit(lx,value) {
+        this.setState({bloading:true});
+        if (lx=='a') {
+            this.fetchHandle(value,'','post'); 
+        }else if (lx=='b') {
+            this.fetchHandle(value,("/"+value.id),'put'); 
+        };
     },
-    fetchHandle(value) {
+    fetchHandle(value,ur,met) {
         req({
-            url: URL,
+            url: URL+ur,
             type: 'json',
-            method: 'post',
+            method: met,
             data: JSON.stringify(value),
             headers:{'x-auth-token':auth.getToken()},
             contentType:'application/json',            
@@ -124,52 +125,17 @@ const zcmxb = React.createClass({
                 title: '系统消息',
                 content: (
                     <div>
-                        <p>操作成功！</p>
+                        <p>操作成功</p>
                     </div>),       
             });
-            this.setState({ add: true, detailHide: true ,update: true})
+            this.setState({bloading:false})
             this.fetchData();
         }).fail(err => {
+            this.setState({bloading:false})
             message.error('Status Code:' + err.status + '  api错误 ')
         })
     },
  
-
-        //点击编辑保存
-    handleSubmit1(value) {
-        let values = value;
-        values.ztbj = '0';
-        this.fetchHandleBB(values);
-       
-    },
-     //点击编辑提交
-    handleOk1(e) {
-       let values = e;
-        values.ztbj = '1'
-        this.fetchHandleBB(values);
-      
-    },
-    fetchHandleBB(value) {     
-        req({
-            url: URL + '/' + value.id,
-            type: 'json',
-            method: 'put',
-            data: JSON.stringify(value),
-            headers:{'x-auth-token':auth.getToken()},
-            contentType:'application/json',
-        }).then(resp => {
-            Modal.success({
-                title: '操作成功',
-            });
-            this.fetchData();
-            this.setState({ add: true, detailHide: true ,update: true})
-        }).fail(err => {
-            message.error('Status Code:' + err.status + '  api错误 ')
-        })
-    },
- 
-
-
     //提交条件查询
     handleSearchSubmit(value) {
         const pager = this.state.pagination;
@@ -181,38 +147,7 @@ const zcmxb = React.createClass({
         };
         this.setState({ pagination: pager, where: value });
         this.fetchData(params);
-        this.setState({ searchToggle: false })
     },
-
-    //点击某行
-    fetchData2(record) {
-        
-        req({
-            url: API_URL + '/' + record.id,
-            type: 'json',
-            method: 'get',
-            headers:{'x-auth-token':auth.getToken()},
-            contentType: 'application/json'
-        }).then(resp => {
-            let entity = entityFormat(resp, entityModel);
-            this.setState({ entity: entity, detailHide: false });
-            
-        }).fail(err => {
-            Modal.error({
-                title: '数据获取错误',
-                content: (
-                    <div>
-                        <p>无法从服务器返回数据，需检查应用服务工作情况</p>
-                        <p>Status: {err.status}</p>
-                    </div>)
-            });
-        })
-    },  
-    //明细表关闭
-    handleDetailClose() {
-        this.setState({ detailHide: true })
-    },
-
 
     //通过API获取数据
     fetchData(params = { page: 1, pageSize: this.state.pagination.pageSize }) {
@@ -235,6 +170,7 @@ const zcmxb = React.createClass({
                 pagination: p,
                 loading: false
             })
+            this.handleViewChange(0);
         }).fail(err => {
             this.setState({ loading: false });
             Modal.error({
@@ -248,144 +184,129 @@ const zcmxb = React.createClass({
         })
     },
 
+    dealWithChanged(key,value){
+        let fs=this.state.fileds;
+        fs[key]=value;
+        fs.zyywcb1=Number(!fs.gzfy1?0:fs.gzfy1)+Number(!fs.flf1?0:fs.flf1)+Number(!fs.jyf1?0:fs.jyf1)
+                            +Number(!fs.ghjf1?0:fs.ghjf1)+Number(!fs.shtc1?0:fs.shtc1)+Number(!fs.bgf1?0:fs.bgf1)
+                            +Number(!fs.clf1?0:fs.clf1)+Number(!fs.hf1?0:fs.hf1)+Number(!fs.pxzlf1?0:fs.pxzlf1)
+                            +Number(!fs.hwf1?0:fs.hwf1)+Number(!fs.zpf1?0:fs.zpf1)+Number(!fs.zj1?0:fs.zj1)
+                            +Number(!fs.zfgjj1?0:fs.zfgjj1)+Number(!fs.gwzxf1?0:fs.gwzxf1)+Number(!fs.qt1?0:fs.qt1);
+        fs.zyywcb=Number(!fs.gzfy?0:fs.gzfy)+Number(!fs.flf?0:fs.flf)+Number(!fs.jyf?0:fs.jyf)
+                            +Number(!fs.ghjf?0:fs.ghjf)+Number(!fs.shtc?0:fs.shtc)+Number(!fs.bgf?0:fs.bgf)
+                            +Number(!fs.clf?0:fs.clf)+Number(!fs.hf?0:fs.hf)+Number(!fs.pxzlf?0:fs.pxzlf)
+                            +Number(!fs.hwf?0:fs.hwf)+Number(!fs.zpf?0:fs.zpf)+Number(!fs.zj?0:fs.zj)
+                            +Number(!fs.zfgjj?0:fs.zfgjj)+Number(!fs.gwzxf?0:fs.gwzxf)+Number(!fs.qt?0:fs.qt);
+        fs.glfy1=Number(!fs.glfy_gzfy1?0:fs.glfy_gzfy1)+Number(!fs.glfy_flf1?0:fs.glfy_flf1)+Number(!fs.glfy_ywzdf1?0:fs.glfy_ywzdf1)
+                            +Number(!fs.glfy_bgf1?0:fs.glfy_bgf1)+Number(!fs.glfy_qtsj1?0:fs.glfy_qtsj1)+Number(!fs.glfy_qcfy1?0:fs.glfy_qcfy1)
+                            +Number(!fs.glfy_zyfxjj1?0:fs.glfy_zyfxjj1)+Number(!fs.glfy_zyzrbx1?0:fs.glfy_zyzrbx1)
+                            +Number(!fs.glfy_clf1?0:fs.glfy_clf1)+Number(!fs.glfy_qtfy1?0:fs.glfy_qtfy1);
+        fs.glfy=Number(!fs.glfy_gzfy?0:fs.glfy_gzfy)+Number(!fs.glfy_flf?0:fs.glfy_flf)+Number(!fs.glfy_ywzdf?0:fs.glfy_ywzdf)
+                            +Number(!fs.glfy_bgf?0:fs.glfy_bgf)+Number(!fs.glfy_qtsj?0:fs.glfy_qtsj)+Number(!fs.glfy_qcfy?0:fs.glfy_qcfy)
+                            +Number(!fs.glfy_zyfxjj?0:fs.glfy_zyfxjj)+Number(!fs.glfy_zyzrbx?0:fs.glfy_zyzrbx)
+                            +Number(!fs.glfy_clf?0:fs.glfy_clf)+Number(!fs.glfy_qtfy?0:fs.glfy_qtfy);
+        fs.zczj1=Number(!fs.zyywsjfj1?0:fs.zyywsjfj1)+Number(!fs.qtywzc1?0:fs.qtywzc1)+Number(!fs.cwfy1?0:fs.cwfy1)
+                            +Number(!fs.yywzc1?0:fs.yywzc1)+Number(fs.zyywcb1)+Number(fs.glfy1);
+        fs.zczj=Number(!fs.zyywsjfj?0:fs.zyywsjfj)+Number(!fs.qtywzc?0:fs.qtywzc)+Number(!fs.cwfy?0:fs.cwfy)
+                            +Number(!fs.yywzc?0:fs.yywzc)+Number(fs.zyywcb)+Number(fs.glfy);
+        this.setState({fileds:fs});
+    },
+
+    onReset(){
+        let fs ={};
+        let record=this.state.entity;
+        for(var key in record){
+            let newkey = key.toLowerCase();
+            fs[newkey] = record[key];
+        }
+        this.setState({ fileds: fs});
+    },
+
+    checkIfND(mp){
+         req({
+            url: URL_C,
+            type: 'json',
+            method: 'get',
+            data: mp,
+            headers:{'x-auth-token':auth.getToken()},
+            contentType:'application/json',            
+        }).then(resp => {
+            if (resp) {
+                return 0;
+            }else{
+               return 1; 
+            };
+        }).fail(err => {
+            return 2;
+        })
+    },
+
     componentDidMount() {
         this.fetchData();
     },
     
-    
-testee(text,record,index){
-
-    var that = this;
-function ddd() {
-   
-     req({
-            url: API_URL + '/' + record.id,
-            type: 'json',
-            method: 'get',
-            headers:{'x-auth-token':auth.getToken()},
-            contentType: 'application/json'
-        }).then(resp => {
-          
-         
-            that.setState({update: !that.state.update,detailHide: true,entity:resp,});
-           
-        }).fail(err => {
-            Modal.error({
-                title: '数据获取错误',
-                content: (
-                    <div>
-                        <p>无法从服务器返回数据，需检查应用服务工作情况</p>
-                        <p>Status: {err.status}</p>
-                    </div>)
-            });
-        })
-   
-}
-function look() {
-    
-     that.fetchData2(record)
-     that.setState({update: true})
-}
-if(record.ZTBJ=="提交"){
-     return ( <span> 
-    <Button disabled size="small" onClick={ddd} >
-    
-    <Icon type="edit" />编辑
-  </Button>
-  <Button size="small" onClick={look} >
-    <Icon type="book" />查看
-  </Button>
-  </span>
-  )
-}else{
-     return ( <span> 
-    <Button   size="small" onClick={ddd} >
-    
-    <Icon type="edit" />编辑
-  </Button>
-  <Button size="small" onClick={look} >
-    <Icon type="book" />查看
-  </Button>
-  </span>
-  )
-}
-   
-},
-
+    rowRender(text,record,index){
+            var that = this;
+             return ( 
+                        <span> 
+                            <Button disabled={record.zt=="提交"?true:false} size="small" onClick={that.handleViewChange.bind(this,2)} ><Icon type="edit" />编辑</Button>
+                            <Button size="small" onClick={that.handleViewChange.bind(this,3)} ><Icon type="book" />查看</Button>
+                      </span> 
+          )
+    },
 
     render() {
-const column1=[
-        {title: '序号', dataIndex: 'key', key: 'key'},       
-        {title: '事务所名称', dataIndex: 'DWMC', key: 'DWMC'},
-        {title: '主营业务成本（单位：元）', dataIndex: 'ZYYWCB', key: 'ZYYWCB'},
-        {title: '年度', dataIndex: 'nd', key: 'nd'},
-        {title: '状态', key: 'ZTBJ', dataIndex: 'ZTBJ'},
-       
-        {
-        title: '操作',
-    key: 'operation',
-     render:this.testee,
-} ];
+        const column=[
+                {title: '序号', dataIndex: 'key', key: 'key'},       
+                {title: '事务所名称', dataIndex: 'DWMC', key: 'DWMC'},
+                {title: '主营业务成本（单位：元）', dataIndex: 'ZYYWCB', key: 'ZYYWCB'},
+                {title: '年度', dataIndex: 'ND', key: 'ND'},
+                {title: '统计时间', dataIndex: 'tjsj', key: 'tjsj',
+                    render(text,record,index){
+                         return <span>{!record.A?null:record.A+" 至 "+record.B}</span>
+                        }},
+                {title: '状态', key: 'zt', dataIndex: 'zt'},
+                {title: '操作', key: 'operation', render:this.rowRender,}
+                ];
         //定义工具栏内容
         let toolbar = <ToolBar>
-            { this.state.add && <Button onClick={this.handleSearchToggle}>
-                <Icon type="search"/>查询
-                { this.state.searchToggle ? <Icon className="toggle-tip" type="circle-o-up"/> :
-                    <Icon className="toggle-tip" type="circle-o-down"/>}
-            </Button>}
-
-            {  this.state.add && <ButtonGroup>
-                <Button type="primary" onClick={this.handleHelper}><Icon type="question"/></Button>
-                <Button type="primary" onClick={this.handleRefresh}><Icon type="reload"/></Button>
-            </ButtonGroup>
-            }
-
-
-            <Button onClick={this.handleAdd}>
-                <Icon type="primary"/>{this.state.add ? "添加" : "返回"}
-                { this.state.add ? <Icon className="toggle-tip" type="plus-square"/> :
-                    <Icon className="toggle-tip" type="arrow-left"/>}
-            </Button>
-            
-
-        </ToolBar>;
-
-        //定义提示内容
+                {this.state.views==0 ? <Button onClick={this.handleViewChange.bind(this,1)}>添加<Icon className="toggle-tip" type="plus-square"/></Button>
+                                                             :<Button onClick={this.handleViewChange.bind(this,0)}>返回<Icon className="toggle-tip" type="arrow-left"/></Button>}
+                { this.state.views==0 && <Button onClick={this.handleSearchToggle}>
+                                                            <Icon type="search"/>查询
+                                                            { this.state.searchToggle ? <Icon className="toggle-tip" type="circle-o-up"/> :
+                                                                <Icon className="toggle-tip" type="circle-o-down"/>}
+                                                            </Button>}
+                {this.state.views==0 && <ButtonGroup>
+                                                            <Button type="primary" onClick={this.handleHelper}><Icon type="question"/></Button>
+                                                            <Button type="primary" onClick={this.handleRefresh}><Icon type="reload"/></Button>
+                                                            </ButtonGroup>}
+                            </ToolBar>;
         let helper = [];
         helper.push(<p key="helper-0">点击查询结果查看支出明细表明细</p>);
         helper.push(<p key="helper-1">也可以添加修改和提交支出明细表</p>);
-        return <div className="cwbb-lrb">
+
+        return <div className="cwbb-zcmx">
             <div className="wrap">
                 {this.state.helper && <Alert message="支出明细表检索查询帮助"
-                    description={helper}
-                    type="info"
-                    closable
-                    onClose={this.handleHelperClose}/>}
-
-
-                <Panel title="支出明细表" toolbar={toolbar}>
+                    description={helper} type="info" closable onClose={this.handleHelperClose}/>}
+                <Panel title={this.state.viewTitle} toolbar={toolbar}>
                     {this.state.searchToggle && <SearchForm
                         onSubmit={this.handleSearchSubmit}/>}
-                    { this.state.add &&  <div className="h-scroll-table">
-
-                        <Table columns={column1}
-                            dataSource={this.state.data}
-                            pagination={this.state.pagination}
-                            loading={this.state.loading}
-                            onChange={this.handleChange}
-                            onRowClick={this.handleRowClick}/>
-                    </div>}
-                    {!this.state.add && <Add onSubmit={this.handleSubmit} handleOk={this.handleOk} data={this.state.data} />}
-                    {!this.state.update && <Panel title="修改"  onClose={this.handleDetailClose}
-                    closable> 
-                    <Update onSubmit={this.handleSubmit1} handleOk={this.handleOk1} data1={this.state.entity} />
-                    </Panel>}
+                    { this.state.views==0 &&  <div className="h-scroll-table">
+                                                        <Table columns={column}
+                                                            dataSource={this.state.data}
+                                                            pagination={this.state.pagination}
+                                                            loading={this.state.loading}
+                                                            onChange={this.handleChange}
+                                                            onRowClick={this.handleRowClick}/>
+                                                        </div>}
+                    {this.state.views==1 && <Add onSubmit={this.handleSubmit.bind(this,'a')}  changed={this.dealWithChanged} 
+                                                                    data={this.state.fileds} loading={this.state.bloading} checkIfND={this.checkIfND} />}
+                    {this.state.views==2 && <Update onSubmit={this.handleSubmit.bind(this,'b')} data={this.state.fileds} 
+                                                                    changed={this.dealWithChanged} loading={this.state.bloading} onReset={this.onReset} />}
+                    {this.state.views==3&&<DetailBox data={this.state.entity}/>}
                 </Panel>
-                {this.state.detailHide ? null : <Panel title="支出明细表明细"
-                    onClose={this.handleDetailClose}
-                    closable>
-                    <DetailBox data={this.state.entity}/>
-                </Panel>}
             </div>
         </div>
     }
