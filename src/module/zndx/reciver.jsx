eb52f8343d50a5a4bdd7e8efe455966e1225d3af
neko <mervyn.chung@foxmail.com>
2016-12-26 21:27:32 +0800

    #1225#01

--- src/module/zndx/reciver.jsx
+++ src/module/zndx/reciver.jsx
@@ -5,10 +5,15 @@ const TabPane = Tabs.TabPane;
 const c = React.createClass({
     getDefaultProps(){
         return {
-            data: {
+            group: {
                 '3': '省内事务所',
                 '114': '外省事务所（无省内分所）',
                 '0':'全部事务所'
+            },
+            yj:{
+                '1':'未缴纳清会费事务所',
+                '2': '未上传财务报表事务所',
+                '3': '未上传表1事务所'
             }
         }
     },
@@ -25,15 +30,15 @@ const c = React.createClass({
         onOk({key: this.state.value, label: data[this.state.value]})
     },
 
-    getRadios(){
-        const {data} = this.props;
+    getGroupRadios(){
+        const {group} = this.props;
         const radioStyle = {
             display: 'block',
             height: '30px',
             lineHeight: '30px'
         };
         let result = [];
-        for (let prop in data) {
+        for (let prop in group) {
             result.push(<Radio style={radioStyle} key={prop} value={prop}>{data[prop]}</Radio>)
         }
         return result
@@ -44,6 +49,11 @@ const c = React.createClass({
         return <Modal {...this.props} onOk={this.handleOk}>
             <Tabs defaultActiveKey="1">
                 <TabPane tab="群组" key="1">
+                    <RadioGroup onChange={this.handleChange} value={this.state.value}>
+                        {this.getGroupRadios()}
+                    </RadioGroup>
+                </TabPane>
+                <TabPane tab="预警通知" key="2">
                     <RadioGroup onChange={this.handleChange} value={this.state.value}>
                         {this.getRadios()}
                     </RadioGroup>
